165c120d2ad8e6d072887c281a795cb1ff12d709
bssd-tuyen <tuyen@bssd.vn>
2021-07-27 10:44:14 +0700

    add ability to generate auto increment ID and customized ID

--- src/Repository/Concerns/CreatesEntity.php
+++ src/Repository/Concerns/CreatesEntity.php
@@ -2,8 +2,10 @@
 
 namespace Bssd\EloquentRepository\Repository\Concerns;
 
+use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @property-read Builder|Model $model
@@ -21,5 +23,36 @@ trait CreatesEntity
     public function create($properties)
     {
         return $this->model->create($properties);
+    }
+
+    /**
+     * Get next auto increment ID
+     *
+     * @return int
+     */
+    public function getAutoIncrementedId(): int
+    {
+        $inspector = DB::select("SHOW TABLE STATUS LIKE '".$this->model->getTable()."'");
+        return $inspector[0]->Auto_increment;
+    }
+
+    /**
+     * Get next customized ID
+     *
+     * @param  string  $field
+     * @param  int     $length
+     * @param  string  $prefix
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function getCustomizedId(string $field, int $length, string $prefix): string
+    {
+        return IdGenerator::generate([
+            'table' => $this->model->getTable(),
+            'field' => $field,
+            'length' => $length,
+            'prefix' => $prefix
+        ]);
     }
 }
